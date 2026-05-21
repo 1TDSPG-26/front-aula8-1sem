9521430c6a780eb9e5530f04fe342d0c22ce5439
alecarlosjesus <profalexandre.jesus@email.com.br>
2026-05-21 09:05:39 -0300

    feat : Realizando a validação com lista.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@email.com.br>

--- js/script.js
+++ js/script.js
@@ -1,107 +1,4 @@
 
-//Objeto INTERNO
-const usuarioDb = {
-    email:"email@email.com",
-    senha:"12345",
-    nome: "José Antonio",
-    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
-}
-
-
-//Capturando o botão de entrar
-const botaoEntrar = document.getElementById("btnEntrar");
-
-//Atrelando ao botão uma função que vai receber dois parâmetros.
-// O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
-
-botaoEntrar.addEventListener("click", function(e){
-    
-    e.preventDefault();
-
-    //Para casa:
-    //Capturar os campos de email e senha.
-    //Armazenar em variáveis.
-    //Imprimir seus valores.
-    const email = document.querySelector("#idEmail");
-    const senha = document.querySelector("#idSenha");
-    
-    //Armazenar os dados que chegam do form em um objeto:
-    const userForm = {
-        email: email.value,
-        senha: senha.value
-    }
-
-    //Capturar o dialog
-    const modalDialog = document.querySelector("#meuModal");
-
-    //Adicionar a mensagem de sucesso para o usuário no modal!
-    //Vamos usar o innerHTML para injetar 2 <p> com o texto na div=msg.
-    //Mas para isso necessitamos capturar a div=msg antes
-    const divMsg = document.querySelector("#msg");
-    
-    //Capturar o botão do modal para atrelar nele o evento de click e assim colocar  neste botão a ação de fechar o modal.
-    const botaoModal = document.querySelector("#btnFechar");
-    botaoModal.addEventListener("click", function(){
-        modalDialog.classList.remove();
-        modalDialog.close();
-    });
-
-    //Validação
-    try {
-
-        if (usuarioDb) {
-
-            if( (usuarioDb.email === userForm.email) && (usuarioDb.senha === userForm.senha)){
-
-                //Abrindo o dialog e deixando o fundo livre.
-                // modalDialog.show();
-
-                //Abrindo o dialog e travando o fundo.
-                modalDialog.showModal();
-                
-                let contador = 5;
-
-                //Injetando os 2 <p> com a mensagem de sucesso.
-                divMsg.innerHTML = `<p>Login realizado com Sucesso!</p><p>Você será redirecionado em ${contador} segundos...</p>`;
-
-                //Temporizador de redirecionamento com setInterval.
-                const intervalo = setInterval( function(){
-                    
-                    contador--;
-                    divMsg.innerHTML = `<p>Login realizado com Sucesso!</p><p>Você será redirecionado em ${contador} segundos...</p>`;
-                    
-                    if(contador === 0){
-                        clearInterval(intervalo);
-                        //Redirecionamento do usuário para uma nova página!!
-                        window.location.href = "../index.html";
-                    }
-
-                }, 1000 );
-
-            }else{
-                throw Error("Email ou Senha incorretos.");
-            }
-            
-        }else{
-            throw Error("Ocorreu um erro no acesso as informações do usuário.");
-        }
-
-    } catch (error) {
-        console.error(error);
-
-            //Abrindo o dialog e travando o fundo.
-            modalDialog.showModal();
-
-            //Adiconando a classe css de erro aos elementos do modal
-            modalDialog.classList.add("danger");
-            botaoModal.classList.add("danger");
-
-            //Injetando os 1 <p> com a mensagem de sucesso.
-            divMsg.innerHTML = `<p>${error}</p>`;
-    }
-});
-
-
 //Pra casa
 //Reutilzar o MODAL para as mensagens de erro!!!
 
@@ -160,3 +57,186 @@ botaoEntrar.addEventListener("click", function(e){
 //     soma("2",2);
 //     console.log( typeof soma("2",2));
 // });
+
+
+
+
+// //Objeto INTERNO
+// const usuarioDb = {
+//     email:"email@email.com",
+//     senha:"12345",
+//     nome: "José Antonio",
+//     avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+// }
+
+
+//Lista de usuários
+const usuarios = [
+  {
+    email: "zarak.vultos@exemplo.com",
+    senha: "12345",
+    nome: "Zarak dos Vultos",
+    idade: 42,
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+  },
+  {
+    email: "elara.astral@exemplo.com",
+    senha: "12345",
+    nome: "Elara Galáxia Astral",
+    idade: 27,
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+  },
+  {
+    email: "mordecai.bruma@exemplo.com",
+    senha: "12345",
+    nome: "Mordecai da Bruma",
+    idade: 51,
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+  },
+  {
+    email: "thallos.ferro@exemplo.com",
+    senha: "12345",
+    nome: "Thallos de Ferro",
+    idade: 35,
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+  },
+  {
+    email: "lyra.eco@exemplo.com",
+    senha: "12345",
+    nome: "Lyra Silvestre Eco",
+    idade: 22,
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+  },
+  {
+    email: "boros.fenda@exemplo.com",
+    senha: "12345",
+    nome: "Boros da Fenda Profunda",
+    idade: 64,
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+  },
+  {
+    email: "vaelin.nimbus@exemplo.com",
+    senha: "12345",
+    nome: "Vaelin Nimbus",
+    idade: 30,
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+  },
+  {
+    email: "selene.quartzo@exemplo.com",
+    senha: "12345",
+    nome: "Selene de Quartzo",
+    idade: 29,
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+  },
+  {
+    email: "kaelthas.sol@exemplo.com",
+    senha: "12345",
+    nome: "Kaelthas Brilho do Sol",
+    idade: 47,
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+  },
+  {
+    email: "joric.martelo@exemplo.com",
+    senha: "12345",
+    nome: "Joric Martelo de Pedra",
+    idade: 38,
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+  }
+];
+
+
+//Capturando o botão de entrar
+const botaoEntrar = document.getElementById("btnEntrar");
+
+//Atrelando ao botão uma função que vai receber dois parâmetros.
+// O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
+
+botaoEntrar.addEventListener("click", function(e){
+    
+    e.preventDefault();
+
+    //Para casa:
+    //Capturar os campos de email e senha.
+    //Armazenar em variáveis.
+    //Imprimir seus valores.
+    const email = document.querySelector("#idEmail");
+    const senha = document.querySelector("#idSenha");
+    
+    //Armazenar os dados que chegam do form em um objeto:
+    const userForm = {
+        email: email.value,
+        senha: senha.value
+    }
+
+    //Capturar o dialog
+    const modalDialog = document.querySelector("#meuModal");
+
+    //Adicionar a mensagem de sucesso para o usuário no modal!
+    //Vamos usar o innerHTML para injetar 2 <p> com o texto na div=msg.
+    //Mas para isso necessitamos capturar a div=msg antes
+    const divMsg = document.querySelector("#msg");
+    
+    //Capturar o botão do modal para atrelar nele o evento de click e assim colocar  neste botão a ação de fechar o modal.
+    const botaoModal = document.querySelector("#btnFechar");
+    botaoModal.addEventListener("click", function(){
+        modalDialog.classList.remove();
+        modalDialog.close();
+    });
+
+    //Validação
+    try {
+
+        if (usuarios) {
+
+            for(let x = 0; x < usuarios.length; x++){
+                
+                if( (usuarios[x].email === userForm.email) && (usuarios[x].senha === userForm.senha)){
+    
+                    //Abrindo o dialog e deixando o fundo livre.
+                    // modalDialog.show();
+    
+                    //Abrindo o dialog e travando o fundo.
+                    modalDialog.showModal();
+                    
+                    let contador = 5;
+    
+                    //Injetando os 2 <p> com a mensagem de sucesso.
+                    divMsg.innerHTML = `<p>Login realizado com Sucesso!</p><p>Você será redirecionado em ${contador} segundos...</p>`;
+    
+                    //Temporizador de redirecionamento com setInterval.
+                    const intervalo = setInterval( function(){
+                        
+                        contador--;
+                        divMsg.innerHTML = `<p>Login realizado com Sucesso!</p><p>Você será redirecionado em ${contador} segundos...</p>`;
+                        
+                        if(contador === 0){
+                            clearInterval(intervalo);
+                            //Redirecionamento do usuário para uma nova página!!
+                            window.location.href = "../index.html";
+                        }
+    
+                    }, 1000 );
+                    return usuarios[x];
+                }
+            }
+                throw Error("Email ou Senha incorretos.");
+                            
+        }else{
+            throw Error("Ocorreu um erro no acesso as informações do usuário.");
+        }
+
+    } catch (error) {
+        console.error(error);
+
+            //Abrindo o dialog e travando o fundo.
+            modalDialog.showModal();
+
+            //Adiconando a classe css de erro aos elementos do modal
+            modalDialog.classList.add("danger");
+            botaoModal.classList.add("danger");
+
+            //Injetando os 1 <p> com a mensagem de sucesso.
+            divMsg.innerHTML = `<p>${error}</p>`;
+    }
+});
+
